8f151fe6196a84c240aa1808783e13c6df17bf02
Estebansito1 <estebansito619@gmail.com>
2026-03-02 15:52:13 -0800

    Add SVM module + smoke test

--- models/svm.py
+++ models/svm.py
@@ -14,3 +14,16 @@ def predict_svm(model, X_test):
     Predict labels using trained SVM model.
     """
     return model.predict(X_test)
+
+if __name__ == "__main__":
+    import numpy as np
+
+    # tiny fake dataset (just to prove it runs)
+    X_train = np.array([[0, 0], [1, 1], [1, 0], [0, 1]])
+    y_train = np.array([0, 1, 1, 0])
+
+    model = train_svm(X_train, y_train, C=1)
+    preds = predict_svm(model, X_train)
+
+    print("SVM ran ✅")
+    print("preds:", preds)
